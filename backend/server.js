28af30af918b8983591bff674765e6e0614f6811
Irosh Deerasinghe <148953843+Deerasinghe@users.noreply.github.com>
2025-02-28 19:50:05 +0530

    Connecting to MONGO DB Atlas

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose
-  .connect(process.env.MONGO_URI || "mongodb://localhost:27017/articlesDB", {
+mongoose.connect("mongodb+srv://netscore51:12345678N@article-hub.ay6bt.mongodb.net/?retryWrites=true&w=majority&appName=Article-Hub", {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB connected successfully!"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+    useUnifiedTopology: true
+}).then(() => console.log("Connected to MongoDB Atlas"))
+.catch(err => console.error("MongoDB connection error:", err));
 
 // Define Schemas
 const articleSchema = new mongoose.Schema({
